10307316a9a9e22133be5dfd580674c30f357e25
Ammar Atiq <ammar.pixako1@gmail.com>
2025-07-14 00:09:05 -0700

    learned how to define class in TS

--- 02_oop/app.js
+++ 02_oop/app.js
@@ -1,1 +1,8 @@
-var a = 12;
+// 1) Class Definition
+// class Device {
+//   name = "Lg";
+//   price = 12000;
+// }
+// let d1 = new Device();
+// let d2 = new Device();
+// type d1 and d2 in browser console to see output
--- 02_oop/app.ts
+++ 02_oop/app.ts
@@ -1,2 +1,11 @@
-let a = 12;
-console.log(a);
+// 1) Class Definition
+
+// class Device {
+//   name = "Lg";
+//   price = 12000;
+// }
+
+// let d1 = new Device();
+// let d2 = new Device();
+
+// type d1 and d2 in browser console to see output
